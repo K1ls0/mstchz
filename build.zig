e918df8feb35fec7d84bd2184a1e4ef06c6e869a
Paul Seydel <Seydel.fnvd@gmail.com>
2025-05-14 13:23:45 +0200

    Added library module as public

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.createModule(.{
+    const lib_mod = b.addModule("mstchz", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
